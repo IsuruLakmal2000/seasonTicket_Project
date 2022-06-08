ab4b3a45316a9a2a025e4911bd44fb6e8f820b30
Pasindu Prabhath <56954364+pasinduPrabhath@users.noreply.github.com>
2022-06-08 13:41:32 +0530

    Add files via upload

---
+++ src/seasonTickets/dbConnection.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package seasonTickets;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+import seasonTickets.sesonTicketUi;
+import seasonTickets.AdminUi;
+
+
+/**
+ *
+ * @author Pasin
+ */
+//public class dbConnection {
+//    try
+//    class.forName("com.mysql.cj.jdbc.Driver");
+//}
